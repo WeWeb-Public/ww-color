e7ffb32d853e9c9d5a7198f4242d14f5be5c516a
Damien DURO <damien@weweb.io>
2019-11-10 13:20:27 +0100

    css cross browser

--- src/wwObjectColor.vue
+++ src/wwObjectColor.vue
@@ -319,8 +319,15 @@ export default {
         top: 0;
         bottom: 0;
         width: 100%;
+        display: -webkit-box;
+        display: -ms-flexbox;
         display: flex;
+        -webkit-box-orient: vertical;
+        -webkit-box-direction: normal;
+        -ms-flex-direction: column;
         flex-direction: column;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
         justify-content: center;
     }
 }
