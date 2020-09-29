c2889c229026ed1c4060a2bdbef36cf69a14f587
Aurélie VIOLETTE <aurelie@weweb.io>
2020-09-29 14:24:40 +0200

    Update with all conventions

---
+++ README.md
@@ -0,0 +1,16 @@
+# WW-COLOR
+
+This an element for Weweb, to display a div with a background.
+This is a very basic componenent, mainly use for background.
+
+## Installation
+
+To run locally, first install all dependencies with `yarn`
+
+## Start
+
+To serve locally, run `yarn serve [PORT]`, and then go to Weweb editor, open developper popup and add localhost:[PORT] as custom wwObject.
+
+## Build
+
+Before release, you can check build error by running `yarn build`
---
+++ src/wwColor.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="ww-color" :style="style"></div>
+</template>
+
+<script>
+export default {
+    name: '__COMPONENT_NAME__',
+    props: {
+        uid: Number,
+        content: Object,
+    },
+    wwDefaultContent: {
+        backgroundColor: 'transparent',
+    },
+    computed: {
+        style() {
+            const { backgroundColor } = this.content;
+            return { background: backgroundColor };
+        },
+    },
+};
+</script>
